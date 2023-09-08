fafd9a4fab3d027ed8757f879f5f47d6e8800bce
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-09-08 10:51:39 -0300

    feat: schema annotation added to task dtos

--- src/main/java/com/matheusdoedev/todolistmanager/api/dto/UpdateTaskDto.java
+++ src/main/java/com/matheusdoedev/todolistmanager/api/dto/UpdateTaskDto.java
@@ -4,14 +4,23 @@ import java.util.Optional;
 
 import com.matheusdoedev.todolistmanager.api.constant.TaskPriorities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Task DTO schema for update task feature body.")
 public class UpdateTaskDto {
+	@Schema(description = "Task name.")
 	private Optional<String> name;
+
+	@Schema(description = "Task description.")
 	private Optional<String> description;
+
+	@Schema(description = "If the task was did or not.")
 	private Optional<Boolean> isDue;
+
+	@Schema(description = "Task priority.")
 	private Optional<TaskPriorities> priority;
 }
